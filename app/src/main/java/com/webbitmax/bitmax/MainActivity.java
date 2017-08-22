26d2092c52e232f95e556f53d250424817fc6b17
Leonardo Sousa <leonpsousa@hotmail.com>
2017-08-21 21:08:21 -0300

    Update MainActivity.java

--- app/src/main/java/com/webbitmax/bitmax/MainActivity.java
+++ app/src/main/java/com/webbitmax/bitmax/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     String teste;
+    String Leon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
