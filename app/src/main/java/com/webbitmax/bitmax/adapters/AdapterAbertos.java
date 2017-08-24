04820e287ecb194b1b8280194f72bd50e85171a7
wargas <wargasteixeira@hotmail.com>
2017-08-23 21:38:58 -0300

    show

--- app/src/main/java/com/webbitmax/bitmax/adapters/AdapterAbertos.java
+++ app/src/main/java/com/webbitmax/bitmax/adapters/AdapterAbertos.java
@@ -80,7 +80,7 @@ public class AdapterAbertos extends RecyclerView.Adapter<AdapterAbertos.MyViewHo
                                 public void onClick(DialogInterface dialog, int which) {
                                     dialog.cancel();
                                 }
-                            });
+                            }).show();
 
                 }
             });
